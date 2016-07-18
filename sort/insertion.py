8a949e425aac3599424d5c20bc5022d5de54bdd7
tylfin <tylfin@gmail.com>
2016-07-18 19:32:17 -0400

    insertion sort

--- sort/insertion.py
+++ sort/insertion.py
@@ -4,9 +4,18 @@ More information see https://en.wikipedia.org/wiki/Insertion_sort
 """
 from math import ceil
 
-def insertion_sort(list_to_sort):
-    pass
+def insertion_sort(sorting_list):
+    for current_index in range(len(sorting_list)):
+        value_to_insert = sorting_list[current_index]
+        index_to_check = current_index - 1
 
+        while (index_to_check >= 0) and (sorting_list[index_to_check] > value_to_insert):
+            sorting_list[index_to_check + 1] = sorting_list[index_to_check]
+            index_to_check -= 1
+
+        sorting_list[index_to_check+1] = value_to_insert
+
+    return sorting_list
 
 def test_insertion_sort():
     simple_test_list = [9, 8, 7, 6, 5, 4, 3, 1, 1, 1, 2, 1]
