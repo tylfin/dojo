98d14d4fd9d6c94ca7158edb745cb122b890a149
tylfin <tylfin@gmail.com>
2016-07-18 18:58:08 -0400

    Remove print

--- sort/merge.py
+++ sort/merge.py
@@ -34,10 +34,9 @@ def merge_sort(list_to_sort):
 
 
 def test_merge_sort():
-    simple_test_list = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+    simple_test_list = [9, 8, 7, 6, 5, 4, 3, 1, 1, 1, 2, 1]
     resulting_list = merge_sort(simple_test_list)
-    expected_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    print(resulting_list)
+    expected_list = [1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     assert(resulting_list == expected_list)
 
 def main():
